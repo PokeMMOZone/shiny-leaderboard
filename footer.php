91270521f9c12d72708337546b2f93af53c03568
Gamer2020 <lostatlife2020@hotmail.com>
2024-07-07 17:39:52 -0400

    update footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 <!-- footer.php -->
 <footer>
     <p>&copy; 2024 PokeMMO Shiny Leaderboard. All rights reserved.</p>
+    <p>Brought to you by <a href="https://pokemmo.zone/">PokeMMO Zone</a>.</p>
+    <p>This website or its creators are in no way affiliated with or endorsed by PokeMMO or its developers.</p>
 </footer>
