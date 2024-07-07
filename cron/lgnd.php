69c733eb65c11a239a8b867cca2cc68236f15ea3
Gamer2020 <lostatlife2020@hotmail.com>
2024-07-07 14:24:47 -0400

    update lgnd to new easier format.

--- cron/lgnd.php
+++ cron/lgnd.php
@@ -29,91 +29,29 @@ libxml_clear_errors();
 // Create a new XPath object
 $xpath = new DOMXPath($dom);
 
-// Find all <p> tags
-$pTags = $xpath->query("//p");
+// Find all div and p tags
+$tags = $xpath->query("//div | //p");
 
-// Exclusion lists
-$usernameExclusionList = ["By JetxLore", "Team LEGEND's OT Shiny Board"];
-$imageUrlExclusionList = [
-    "https://forums.pokemmo.com/uploads/monthly_2022_06/secret_shiny_particle.png.11a9039aa8014d4afbd7e50a542b631c.png",
-    "https://forums.pokemmo.com/uploads/monthly_2022_10/image.png.bbfb8af0def75a91b364007b219e736c.png",
-    "https://archives.bulbagarden.net/media/upload/e/eb/Bag_Safari_Ball_Sprite.png",
-    "https://forums.pokemmo.com/uploads/monthly_2024_06/shinywars2.thumb.png.18f54273d58f7b6ed22e8e81c023e422.png"
-];
-
-// Lookup list for special cases like 'x4'
-$usernameLookupList = [
-    "x2" => 1,
-    "x3" => 2,
-    "x4" => 3,
-    "x5" => 4,
-    "x6" => 5,
-    "x7" => 6,
-    "x8" => 7,
-];
-
+// Initialize arrays
 $users = [];
-$imageUrls = [];
-$currentUsername = "";
-$lastValidUsername = "";
 
-// Loop through each <p> tag
-foreach ($pTags as $pTag) {
-    // Check if the <p> tag contains a username
-    $usernameNode = $xpath->query(".//span/strong | .//span/b | .//span/font", $pTag);
-    if ($usernameNode->length > 0) {
-        $potentialUsername = $usernameNode->item(0)->textContent;
+// Regular expression pattern to match usernames and counts
+$usernamePattern = '/([a-zA-Z0-9]+)\s*\((\d+)\)/';
+
+// Loop through each tag
+foreach ($tags as $tag) {
+    $tagContent = $dom->saveHTML($tag);
+    
+    // Check if the tag contains a username
+    if (preg_match($usernamePattern, $tagContent, $usernameMatches)) {
+        $username = trim($usernameMatches[1]);
+        $imageCount = intval($usernameMatches[2]);
         
-        if (!in_array($potentialUsername, $usernameExclusionList)) {
-            if (isset($usernameLookupList[$potentialUsername])) {
-                if ($lastValidUsername != "") {
-                    // Do nothing, special case handled
-                }
-            } else {
-                $currentUsername = $potentialUsername;
-                $lastValidUsername = $currentUsername;
-                if (!isset($users[$currentUsername])) {
-                    $users[$currentUsername] = [
-                        'imageCount' => 0,
-                        'images' => []
-                    ];
-                }
-            }
-        } else {
-            // Do not reset current username if it's in the exclusion list
-            $currentUsername = "";
+        if (!isset($users[$username])) {
+            $users[$username] = [
+                'imageCount' => $imageCount
+            ];
         }
-    }
-
-    // If there's a last valid username, look for images and multipliers in the current <p> tag
-    if ($lastValidUsername != "") {
-        $images = $xpath->query(".//img", $pTag);
-        $textNodes = $xpath->query(".//span/strong | .//span/b | .//span/font | .//strong | .//b | .//font", $pTag);
-
-        // Get all image URLs
-        foreach ($images as $image) {
-            $imgUrl = $image->getAttribute("src");
-            if (!in_array($imgUrl, $imageUrlExclusionList)) {
-                $imageUrls[] = $imgUrl;
-                $users[$lastValidUsername]['images'][] = $imgUrl;
-                $users[$lastValidUsername]['imageCount']++;
-            }
-        }
-
-        // Check for special cases in text nodes
-        foreach ($textNodes as $textNode) {
-            $textContent = $textNode->textContent;
-            if (isset($usernameLookupList[$textContent])) {
-                $users[$lastValidUsername]['imageCount'] += $usernameLookupList[$textContent];
-            }
-        }
-    }
-}
-
-// Remove users with no images
-foreach ($users as $username => $data) {
-    if ($data['imageCount'] == 0) {
-        unset($users[$username]);
     }
 }
 
@@ -152,12 +90,7 @@ file_put_contents($file, json_encode($jsonData, JSON_PRETTY_PRINT));
 echo "<h1>Team LGNDS OT Shiny Board</h1>";
 echo "<ul>";
 foreach ($users as $username => $data) {
-    echo "<li><strong>$username</strong>: {$data['imageCount']} images";
-    echo "<ul>";
-    foreach ($data['images'] as $url) {
-        echo "<li>$url</li>";
-    }
-    echo "</ul></li>";
+    echo "<li><strong>$username</strong>: {$data['imageCount']} shinies</li>";
 }
 echo "</ul>";
 ?>
