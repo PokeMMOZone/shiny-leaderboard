43f3d7397d83f0bbd898933f79c4495ba023b1fe
Gamer2020 <lostatlife2020@hotmail.com>
2024-07-08 20:13:09 -0400

    Change text in nav menu.

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="player-leaderboard.php">Player Shiny OT Leaderboard</a></li>
+        <li><a href="player-leaderboard.php">Player Leaderboard</a></li>
         <li><a href="faq.php">FAQ</a></li>
         <li><a href="about.php">About</a></li>
         <!-- Add more pages as needed -->
