f16fa9e977cc99a60474a1f9caf7b34cf454281d
Gamer2020 <lostatlife2020@hotmail.com>
2024-11-21 13:32:01 -0500

    test commit.

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $totalTeams = count($teams);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PokeMMO Shiny OT Team Leaderboard</title>
+    <title>PokeMMO Shiny OT Team Leaderboard - test</title>
     <link rel="icon" type="image/png" href="icon.png">
     <?php include 'styles-scripts.php'; ?>
 </head>
