c66c178e687ec66998864f0c67c4250ed06cce02
Gamer2020 <lostatlife2020@hotmail.com>
2025-03-04 18:18:08 -0500

    Add mesage to site regardning shiny board.

--- nav.php
+++ nav.php
@@ -8,3 +8,7 @@
         <!-- Add more pages as needed -->
     </ul>
 </nav>
+
+<div id="flashing-message">
+    <a href="https://pokemmo.zone/2025/03/shiny-board-teaser-screenshots/" target="_blank">Click here for info on the new website! We will not be adding any new teams to this website and all focus is going into releasing Shiny Board.</a>
+</div>
